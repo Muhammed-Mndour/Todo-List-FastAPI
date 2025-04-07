d80ae56a0f6830fd3a62df97c9e9df375c557d50
mabdelhady <darsh.m7md@gmail.com>
2025-04-08 01:06:14 +0200

    Fix health_router

--- src/apptodolist/views/routers.py
+++ src/apptodolist/views/routers.py
@@ -5,6 +5,6 @@ from .v1.routers import router as router_v1
 from .v2.routers import router as router_v2
 
 router = APIRouter()
-router.include_router(health_router, prefix='/', tags=['system'])
+router.include_router(health_router, tags=['system'])
 router.include_router(router_v1, prefix='/v1', tags=['v1'])
 router.include_router(router_v2, prefix='/v2', tags=['v2'])
