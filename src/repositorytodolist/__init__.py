fbd109255f0738aa49a73a6508be33ae882ca64b
Muhammad Al-Halaby <moj1323@gmail.com>
2025-05-07 00:27:48 +0300

    Add tables.py for todolist

---
+++ src/repositorytodolist/__init__.py
@@ -0,0 +1,3 @@
+from utils.engines import get_engine
+
+engine_todolist = get_engine('todolist')
---
+++ src/repositorytodolist/models/tables.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+from repositorytodolist import engine_todolist
+from utils import util
+
+
+def create_all():
+    Base.metadata.create_all(engine_todolist)
+
+
+def recreate_all():
+    assert util.IS_DEV, 'must be dev'
+    Base.metadata.drop_all(engine_todolist)
+    Base.metadata.create_all(engine_todolist)
+
+
+Base = declarative_base()
+
+
+class Model(Base):
+    __abstract__ = True
+    __bind_key__ = 'todolist'
