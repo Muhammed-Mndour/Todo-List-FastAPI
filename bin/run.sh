44daa11205f54d6d07fcc7d48fd0e5df96e18f17
Muhammad Al-Halaby <moj1323@gmail.com>
2025-05-10 16:04:22 +0300

    Support multiple run commands in run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-gunicorn --log-level debug --reload --bind 0.0.0.0:8080 --timeout=15 --worker-class=uvicorn.workers.UvicornWorker --keep-alive 60 --workers 4 apptodolist.web:app
+if test "x${APPNAME}" = "xapptodolist" ; then
+  gunicorn --log-level debug --reload --bind 0.0.0.0:8080 --timeout=15 --worker-class=uvicorn.workers.UvicornWorker --keep-alive 60 --workers 4 apptodolist.web:app
+else
+  echo "Invalid \$APPNAME '${APPNAME}'"
+  exit 1
+fi
